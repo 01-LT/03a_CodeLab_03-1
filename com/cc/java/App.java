bee47081d8c9fd3262f615672b9f815aa975b04b
Lawrence <lawrence.langen@gmail.com>
2023-02-03 13:02:27 +0100

    change1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,16 @@ public class App {
     
     public static void main(String[] args) {
      
+        MA_Accounting acc1=new MA_Accounting("Wilkonson", "Jack", 1984);
+        MA_Accounting acc2=new MA_Accounting("Hepburn", "Audrey", 1948);
+        
+        pollAccounting(acc1);
+        pollAccounting(acc2);
+
+    }
+
+    private static void pollAccounting(MA_Accounting ma) {
+        output(ma.doYourWork("#firstName"));
     }
 
     private static void output(String outputStr) {
@@ -13,4 +23,5 @@ public class App {
 
 
 }
+
 
